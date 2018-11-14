8ea5564a536ebdb25c0b1ac56c5ea413bf96f0c2
Jaivonah Adside <jjtheartist23@gmail.com>
2018-11-14 23:15:42 +0000

    randomly sets

--- js/script.js
+++ js/script.js
@@ -8,4 +8,26 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+$("button").click(function() { 
+    var userChoice = $("#input").val();
+    $("#userChoice").text(userChoice);
+    var randomChoice = Math.random();
+    
+    if(randomChoice<.333){
+        var computerChoice=" rock"
+        $("#text").html(computerChoice);
+    }
+    else if(randomChoice<.666){
+        var computerChoice=" paper"
+        $("#text").html(computerChoice);
+    }
+   else if(randomChoice<.){
+        var computerChoice=" rock"
+        $("#text").html(computerChoice);
+    }
+});
+ 
 
+
+var computerChoice = " "
+var winner = " "
